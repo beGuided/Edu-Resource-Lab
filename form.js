9ead64a98e1fe837ec839a8105b009b34c6f1e73
orierowisdom <gwlgroups@gmail.com>
2021-05-12 00:24:21 +0100

    first commit

---
+++ form.js
@@ -0,0 +1,119 @@
+
+// const Tname = document.querySelector(".name-input > h6");
+
+// var formIdd = document.getElementById("formId").elements.namedItem
+// function myNewFunction() {
+//     var x = formIdd("Newname").value;
+//     Tname.innerHTML = x;
+// }
+
+
+
+// function changee(){
+
+// //    document.getElementsByClassName("onit").addEventListener("click", function(){
+  
+//      testt.innerText;
+// }
+
+// const testt = document.getElementById("tool");
+// function tryy() {
+//    var y = addEventListener("click", ()=>{
+//         document.getElementById("onit").innerText;
+//         alert(stopl);
+//         console.log(hello)
+//     })
+//     testt.innerText = y;
+// }
+
+// alert(
+//     tryy(90));
+
+
+
+
+
+
+
+// function submitHandle(){
+//     window.addEventListener("load", ()=>{
+//         const params = new URL(document.location).searchParams;
+//         const Fname = params.getElementById("formm").elements.namedItem("fullname").value;
+
+//         var Fname =document.getElementById("formm").elements.namedItem("fullname").value;
+    
+        
+//          document.getElementById("fullnames").innerHTML = Fname;
+//     })
+// }
+
+// function fee(){
+//     const Fname =document.getElementById("formm").elements.namedItem("fullname").value;
+//     document.getElementById("fullnames").innerHTML = Fname;
+
+//     localStorage.setItem("NAME", Fname);
+
+//     return;
+// }
+
+
+// document.getElementById("fullnames");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myFunction() {
+//     var fMatric = document.getElementById("matric").elements.value;
+//     var fLevel = document.getElementById("level").elements.value;
+//     var fDepartment = document.getElementById("department").elements.value;
+//     var fEmail = document.getElementById("email").elements.value;
+//     var fSex = document.getElementById("sex").elements.value;
+//     var fSexl = document.getElementsByClassName("sex").elements.value;
+//     var fNumber = document.getElementById("number").elements.value;
+//     var fSemester = document.getElementById("semester").elements.value;
+//     var fSession = document.getElementById("session").elements.value;
+
+//     document.getElementsByClassName("sex").innerHTML =fSexl ;
+    
+// }
+
+
+
+
+// var formId = document.getElementById("myForm").elements.namedItem
+// function myFunction() {
+//     var x = formId("fname").value;
+//     document.getElementById("demo").innerHTML = x;
+// }
+
+
+
+
+//             // [7:10 AM, 4/27/2021] GWL Services: For the image
+//             // [7:10 AM, 4/27/2021] GWL Services: <p><input type="file" accept="image/*" name="image" id="file"
+//             //         onchange="loadFile(event)" style="display: none;"></p>
+//             // <p><label for="file" style="cursor: pointer;">Upload Image</label></p>
+//             // <p><img id="output" width="200" /></p>
+//             // <script> var loadFile = function (event) { var image = document.getElementById('output'); image.src = URL.createObjectURL(event.target.files[0]); }; </script>
